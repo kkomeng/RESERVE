7b87f69839b8e5cae44fa4c26a4367c4482a819c
kkomeng <komix68@gmail.com>
2023-11-04 01:49:06 +0900

    DB登録処理実装

--- config/config.php
+++ config/config.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL & ~E_WARNING);
 
 ini_set('date.timezone', 'Asia/Tokyo');
 
-define('DB_HOST', 'reserve-mysql-1');
+define('DB_HOST', 'localhost:3306');
 define('DB_NAME', 'reserve');
 define('DB_USER', 'root');
 define('DB_PASSWORD', 'password');
